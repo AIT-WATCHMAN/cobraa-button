726d103e363ff1f8bd405cad92e75ee89610b4fa
lewis <lewissexton2396@gmail.com>
2025-01-23 21:14:13 +0000

    Added Mn decay chain and encapsulation rates

--- cobraa/io_operations.py
+++ cobraa/io_operations.py
@@ -169,7 +169,7 @@ def generateJobs():
     singlesscript = f"{dir}/script{additionalString}_singles.sh".replace(" ","")
     outfile_singlesscript = open(singlesscript, "w+")
     outfile_singlesscript.writelines(f"""#!/bin/sh
-source {'~/.bashrc'} && TMPNAME=$(date +%s%N)  && rat mac/detector_{detectorStr}.mac mac/process.mac """)
+source {ratDir+'/../../env.sh'} && TMPNAME=$(date +%s%N)  && rat mac/detector_{detectorStr}.mac mac/process.mac """)
     for _p in proc:
         for _loc in proc[_p]:
             for _element in d[_p][_loc]:
@@ -178,7 +178,7 @@ source {'~/.bashrc'} && TMPNAME=$(date +%s%N)  && rat mac/detector_{detectorStr}
                     script = f"{dir}/script{additionalString}_{_element}_{_loc}_{_p}.sh".replace(" ","")
                     outfile_script = open(script,"w+")
                     outfile_script.writelines(f"""#!/bin/sh
-source {'~/.bashrc'} && TMPNAME=$(date +%s%N)  && rat mac/detector_{detectorStr}.mac mac/process.mac mac/phys_{_element}.mac mac/geo_{_loc}.mac mac/rates_{_element}_{_loc}_{_p}.mac mac/evts_{_element}_{_loc}_{_p}.mac -o root_files{additionalString}/{_element}_{_loc}_{_p}/run$TMPNAME.root -l log{additionalString}/{_element}_{_loc}_{_p}/run$TMPNAME.log""")
+source {ratDir+'/../../env.sh'} && TMPNAME=$(date +%s%N)  && rat mac/detector_{detectorStr}.mac mac/process.mac mac/phys_{_element}.mac mac/geo_{_loc}.mac mac/rates_{_element}_{_loc}_{_p}.mac mac/evts_{_element}_{_loc}_{_p}.mac -o root_files{additionalString}/{_element}_{_loc}_{_p}/run$TMPNAME.root -l log{additionalString}/{_element}_{_loc}_{_p}/run$TMPNAME.log""")
                     outfile_script.close
                     os.chmod(script,S_IRWXU)
                     file = f"{dir}/job{additionalString}_{_element}_{_loc}_{_p}.sh".replace(" ","")
@@ -202,7 +202,7 @@ source {'~/.bashrc'} && TMPNAME=$(date +%s%N)  && rat mac/detector_{detectorStr}
                         script = f"{dir}/script{additionalString}_{_element}_{_loc}_{_p}.sh".replace(" ","")
                         outfile_script = open(script,"w+")
                         outfile_script.writelines(f"""#!/bin/sh
-    source {'~/.bashrc'} && TMPNAME=$(date +%s%N)  && rat mac/detector_{detectorStr}.mac mac/process.mac mac/phys_{_element}.mac mac/geo_{_loc}.mac mac/rates_{_element}_{_loc}_{_p}.mac mac/evts_{_element}_{_loc}_{_p}.mac -o root_files{additionalString}/{_element}_{_loc}_{_p}/run$TMPNAME.root -l log{additionalString}/{_element}_{_loc}_{_p}/run$TMPNAME.log""")
+    source {ratDir+'/../../env.sh'} && TMPNAME=$(date +%s%N)  && rat mac/detector_{detectorStr}.mac mac/process.mac mac/phys_{_element}.mac mac/geo_{_loc}.mac mac/rates_{_element}_{_loc}_{_p}.mac mac/evts_{_element}_{_loc}_{_p}.mac -o root_files{additionalString}/{_element}_{_loc}_{_p}/run$TMPNAME.root -l log{additionalString}/{_element}_{_loc}_{_p}/run$TMPNAME.log""")
                         outfile_script.close
                         os.chmod(script,S_IRWXU)
                         file = f"{dir}/job{additionalString}_{_element}_{_loc}_{_p}.sh".replace(" ","") 
@@ -271,7 +271,6 @@ def mergeRootFiles():
                             if arguments['--core']:
                                 os.system(f'hadd -f -k -v 0 core_{outfile} core_{files}')
                             else:
-                                print("fred_",outfile)
                                 os.system(f'hadd -f -k -v 0 fred_{outfile} fred_{files}')
 
 
@@ -443,7 +442,6 @@ getenv     = True
 queue {nruns}
 
 """
-#nruns in replace of 1 (line 442)
     elif arguments['--cluster']=='glasgow':
         jobheader = f"""#!/bin/sh
 
@@ -473,7 +471,6 @@ qsub -t 1-40 -V -q ppe.7.day -N job_{_element} -j y -cwd {script}
 
 srun -n{nruns} {script}
     """
-# nruns in replace of 1 (line 472)
     else:
         jobheader = f"""#!/bin/sh
 
