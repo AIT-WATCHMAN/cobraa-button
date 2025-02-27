046fa0f858c5ae7596f1217f852486f7ad0730aa
Lewis Sexton <lewissexton2396@gmail.com>
2025-02-27 13:52:31 +0000

    Updating .gitignore to stop tracking unwanted files

--- env_cobraa.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-source /home/ph1lse/ratpac-setup/env.sh
-COBRAAENV=/home/ph1lse/cobraa-button
-PATH=$COBRAAENV:$PATH
-PYTHONPATH=$COBRAAENV:$PYTHONPATH
-export COBRAAENV PATH PYTHONPATH
-function cobraa { python3 $COBRAAENV/cobraa.py $*; }
